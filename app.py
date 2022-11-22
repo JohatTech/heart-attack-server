c5d5685eea04ffc31f25444f4c054cf3fe769844
JohatTech <johat.abrego24@gmail.com>
2022-11-22 11:51:02 -0500

    some changes in the app file

--- app.py
+++ app.py
@@ -33,13 +33,10 @@ def predict():
 		return ('No model here to use')
 # this is for run the app yeah just for run
 if __name__== "__main__":
-	try:
-		port = int(sys.argv[1]) # This is for a command-line input
-	except:
-		port = 5000 # If you don't provide any port the port will be set to 12345
-		model = jb.load("SVM.pkl") # Load "model.pkl"
-		print ('Model loaded')
-		model_columns = jb.load("models_columns.pkl") # Load "model_columns.pkl"
-		print ('Model columns loaded')
-		app.run(port=port, host="0.0.0.0", debug=False)
+	port = 5000 # If you don't provide any port the port will be set to 12345
+	model = jb.load("SVM.pkl") # Load "model.pkl"
+	print ('Model loaded')
+	model_columns = jb.load("models_columns.pkl") # Load "model_columns.pkl"
+	print ('Model columns loaded')
+	app.run(port=port, host="0.0.0.0", debug=False)
     
